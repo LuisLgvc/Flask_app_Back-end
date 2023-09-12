4f9c9b79fe18cfe1c557e9b38dbb569a7a866224
LuisLgvc <113144443+LuisLgvc@users.noreply.github.com>
2023-09-11 23:36:25 -0300

    Se agregan los endpoints para los mensajes y tambien se agrega el contenido del __init__.py

---
+++ api/routes/messages_bp.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+from ..controllers.messages_controller import MessagesController
+
+messages_bp = Blueprint('messages_bp', __name__)
+
+messages_bp.route('/<int:id_canal>', methods=['GET'])(MessagesController.get_messages)
+messages_bp.route('/', methods=['POST'])(MessagesController.create_message)
+messages_bp.route('/<int:id_message>', methods=['PUT'])(MessagesController.update_message)
+messages_bp.route('/<int:id_message>', methods=['DELETE'])(MessagesController.delete_message)
