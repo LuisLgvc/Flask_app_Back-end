04be8395be598c43b2aa583784dce36f4afd6833
MarceGuil <143463053+MarceGuil@users.noreply.github.com>
2023-09-13 17:16:10 -0300

    Update usuario.py
    Se incorporo la logica del Usuario. En mismo archivo tenemos la creacion del nuevo usuario y la logica del Usuario.

--- api/models/usuario.py
+++ api/models/usuario.py
@@ -1,6 +1,5 @@
-#Creacion de Modelo Usuario 
-
-from flask import jsonify
+#Creacion del Modelo Usuario
+import mysql.connector
 from database import DatabaseConnection
 
 class Usuario:
@@ -17,8 +16,8 @@ class Usuario:
         self.status_id = kwargs.get('status_id')
         self.avatar_url = kwargs.get('avatar_url', None) 
 
+# Serializa el objeto Usuario en un diccionario
     def serialize(self):
-        # Serializa el objeto Usuario en un diccionario
         user_dict = {
             'user_id': self.user_id,
             'username': self.username,
@@ -33,29 +32,88 @@ class Usuario:
             'avatar_url': self.avatar_url
         }
         return user_dict
+    
+#Logica del Modelo Usuario 
+#Creacion de un nuevo Usuario 
+    @classmethod
+    def crear_usuario(cls, usuario):
+        try:
+            conn = DatabaseConnection.connect() 
+            cursor = conn.cursor()
 
-# Crear un usuario usando **kwargs
-kwargs = {
-    'user_id': 1,
-    'username': 'ejemplo_usuario',
-    'password': 'contraseña123',
-    'email': 'usuario@ejemplo.com',
-    'first_name': 'Nombre',
-    'last_name': 'Apellido',
-    'date_of_birth': '1990-01-01',
-    'creation_date': '2023-09-08',
-    'last_login': '2023-09-08',
-    'status_id': 1,
-    'avatar_url': 'https://example.com/avatar.png'
-}
+            insert_query = "INSERT INTO usuarios (username, password, email, first_name, last_name, date_of_birth, creation_date, last_login, status_id, avatar_url) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            values = (usuario.username, usuario.password, usuario.email, usuario.first_name, usuario.last_name, usuario.date_of_birth, usuario.creation_date, usuario.last_login, usuario.status_id, usuario.avatar_url)
 
-usuario = Usuario(**kwargs)
+            cursor.execute(insert_query, values)
+            conn.commit()
 
-# Serializar el usuario a un diccionario
-usuario_dict = usuario.serialize()
+            user_id = cursor.lastrowid
+            cursor.close()
+            conn.close()
 
-# Imprimir el diccionario serializado
-print(usuario_dict)
+            return user_id
+        except mysql.connector.Error as error:
+            print(f"Error al crear el usuario: {error}")
+            return None
+        
+#Obtener un nuevo usuario por ID 
+    @classmethod
+    def obtener_usuario_por_id(cls, user_id):
+        try:
+            conn = DatabaseConnection.connect()
+            cursor = conn.cursor(dictionary=True)
+
+            select_query = "SELECT * FROM usuarios WHERE user_id = %s"
+            cursor.execute(select_query, (user_id,))
+            usuario = cursor.fetchone()
+
+            cursor.close()
+            conn.close()
+
+            return usuario
+        except mysql.connector.Error as error:
+            print(f"Error al obtener el usuario: {error}")
+            return None
+        
+#Actualizar un Usuario 
+@classmethod
+def actualizar_usuario(cls, user_id, nuevos_datos):
+    try:
+        conn = DatabaseConnection.connect()
+        cursor = conn.cursor()
+
+        update_query = "UPDATE usuarios SET username=%s, password=%s, email=%s, first_name=%s, last_name=%s, date_of_birth=%s, creation_date=%s, last_login=%s, status_id=%s, avatar_url=%s WHERE user_id=%s"
+        values = (nuevos_datos['username'], nuevos_datos['password'], nuevos_datos['email'], nuevos_datos['first_name'], nuevos_datos['last_name'], nuevos_datos['date_of_birth'], nuevos_datos['creation_date'], nuevos_datos['last_login'], nuevos_datos['status_id'], nuevos_datos['avatar_url'], user_id)
+
+        cursor.execute(update_query, values)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return True
+    except mysql.connector.Error as error:
+        print(f"Error al actualizar el usuario: {error}")
+        return False
+
+#Eliminar un contacto 
+@classmethod
+def eliminar_usuario(cls, user_id):
+    try:
+        conn = DatabaseConnection.connect()
+        cursor = conn.cursor()
+
+        delete_query = "DELETE FROM usuarios WHERE user_id=%s"
+        cursor.execute(delete_query, (user_id,))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return True
+    except mysql.connector.Error as error:
+        print(f"Error al eliminar el usuario: {error}")
+        return False
 
 
 
