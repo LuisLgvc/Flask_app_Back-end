514669ec3ed9f33537e73ca7fe193ef137cc4906
MarceGuil <143463053+MarceGuil@users.noreply.github.com>
2023-09-16 05:16:52 -0300

    Excepciones Modelo Usuario

--- api/models/exceptions.py
+++ api/models/exceptions.py
@@ -18,3 +18,19 @@ class CustomException(Exception):
         })
         response.status_code = self.status_code
         return response
+    
+class UsuarioNoEncontrado(CustomException):
+    def __init__(self, description='Usuario no encontrado'):
+        super().__init__(status_code=404, name='Usuario No Encontrado', description=description)
+
+class CrearUsuarioError(CustomException):
+    def __init__(self, description='Error al crear el usuario'):
+        super().__init__(status_code=400, name='Crear Usuario Error', description=description)
+
+class ActualizarUsuarioError(CustomException):
+    def __init__(self, description='Error al actualizar el usuario'):
+        super().__init__(status_code=400, name='Actualizar Usuario Error', description=description)
+
+class EliminarUsuarioError(CustomException):
+    def __init__(self, description='Error al eliminar el usuario'):
+        super().__init__(status_code=400, name='Eliminar Usuario Error', description=description)
