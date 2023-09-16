16163b8d6ca7128360c76a2f79486b1c6209d08e
MarceGuil <143463053+MarceGuil@users.noreply.github.com>
2023-09-16 11:14:37 -0300

    Update exceptions.py
    Se agrega excepciones de modelo Usuario y de Modelo Servidor

--- api/models/exceptions.py
+++ api/models/exceptions.py
@@ -18,3 +18,42 @@ class CustomException(Exception):
         })
         response.status_code = self.status_code
         return response
+#Excepciones Modelo Usuario     
+class UsuarioNoEncontrado(CustomException):
+    def __init__(self, description='Usuario no encontrado'):
+        super().__init__(status_code=404, name='Usuario No Encontrado', description=description)
+
+class CrearUsuarioError(CustomException):
+    def __init__(self, description='Error al crear el usuario'):
+        super().__init__(status_code=400, name='Crear Usuario Error', description=description)
+
+class ActualizarUsuarioError(CustomException):
+    def __init__(self, description='Error al actualizar el usuario'):
+        super().__init__(status_code=400, name='Actualizar Usuario Error', description=description)
+
+class EliminarUsuarioError(CustomException):
+    def __init__(self, description='Error al eliminar el usuario'):
+        super().__init__(status_code=400, name='Eliminar Usuario Error', description=description)
+
+#Excepciones Modelo Servidor 
+class ServerNotFoundException(Exception):
+    def __init__(self, server_id):
+        super().__init__()
+        self.server_id = server_id
+
+    def get_response(self):
+        return jsonify({"message": f"Servidor con ID {self.server_id} no encontrado"}), 404
+
+class ServerUpdateException(Exception):
+    def __init__(self):
+        super().__init__()
+
+    def get_response(self):
+        return jsonify({"message": "Error al actualizar el servidor"}), 500
+
+class ServerDeleteException(Exception):
+    def __init__(self):
+        super().__init__()
+
+    def get_response(self):
+        return jsonify({"message": "Error al eliminar el servidor"}), 500       
