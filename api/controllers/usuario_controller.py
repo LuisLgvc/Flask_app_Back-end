4c1eee80572043012f8dc2a079b0016fa6c69363
MarceGuil <143463053+MarceGuil@users.noreply.github.com>
2023-09-16 05:11:33 -0300

    Update usuario_controller.py

--- api/controllers/usuario_controller.py
+++ api/controllers/usuario_controller.py
@@ -1,46 +1,55 @@
-from flask import Blueprint, request, jsonify
-from models import Usuario
+from flask import jsonify, request
+from ..models import Usuario
 
-usuario_bp = Blueprint('usuario_bp', __name__)
+class UsuarioController:
+    
+    @classmethod #Endpoint de Prueba http://127.0.0.1:5000/api/usuarios METODO POST
+    def crear_usuario(cls):
+        data = request.json
+        nuevo_usuario = Usuario(
+            username=data.get('username'),
+            password=data.get('password'),
+            email=data.get('email'),
+            first_name=data.get('first_name'),
+            last_name=data.get('last_name'),
+            date_of_birth=data.get('date_of_birth'),
+            avatar_url=data.get('avatar_url')
+        )
 
-@usuario_bp.route('/usuarios', methods=['POST'])
-def crear_usuario():
-    data = request.json
-    nuevo_usuario = Usuario(**data)
-    user_id = Usuario.crear_usuario(nuevo_usuario)
-    if user_id is not None:
-        return jsonify({'user_id': user_id}), 201
-    return jsonify({'message': 'Error al crear el usuario'}), 500
+        user_id = Usuario.crear_usuario(nuevo_usuario)
 
-@usuario_bp.route('/usuarios/<int:user_id>', methods=['GET'])
-def obtener_usuario(user_id):
-    usuario = Usuario.obtener_usuario_por_id(user_id)
-    if usuario:
-        return jsonify(usuario.serialize()), 200
-    return jsonify({'message': 'Usuario no encontrado'}), 404
+        if user_id:
+            return jsonify({"user_id": user_id}), 201
+        else:
+            return jsonify({"message": "Error al crear el usuario"}), 500
 
-@usuario_bp.route('/usuarios/<int:user_id>', methods=['PUT'])
-def actualizar_usuario(user_id):
-    data = request.json
-    usuario = Usuario.obtener_usuario_por_id(user_id)
-    if usuario:
-        usuario.username = data.get('username', usuario.username)
-        usuario.password = data.get('password', usuario.password)
-        usuario.email = data.get('email', usuario.email)
-        usuario.first_name = data.get('first_name', usuario.first_name)
-        usuario.last_name = data.get('last_name', usuario.last_name)
-        usuario.date_of_birth = data.get('date_of_birth', usuario.date_of_birth)
-        usuario.creation_date = data.get('creation_date', usuario.creation_date)
-        usuario.last_login = data.get('last_login', usuario.last_login)
-        usuario.status_id = data.get('status_id', usuario.status_id)
-        usuario.avatar_url = data.get('avatar_url', usuario.avatar_url)
+    @classmethod #Endpoint de Prueba http://127.0.0.1:5000/api/usuarios METODO GET
+    def get_usuarios(cls):
+        usuarios = Usuario.obtener_usuarios()
+        if usuarios:
+            return jsonify([usuario.serialize() for usuario in usuarios]), 200
+        else:
+            return jsonify({"message": "No se encontraron usuarios"}), 404
+    
+    @classmethod #Endpoint de Prueba http://127.0.0.1:5000/api/usuarios/1 METODO GET
+    def obtener_usuario_por_id(cls, user_id):
+        usuario = Usuario.obtener_usuario_por_id(user_id)
+        if usuario:
+            return jsonify(usuario.serialize()), 200
+        else:
+            return jsonify({"message": "Usuario no encontrado"}), 404
 
-        if Usuario.actualizar_usuario(usuario):
-            return jsonify({'message': 'Usuario actualizado'}), 200
-    return jsonify({'message': 'Usuario no encontrado o error al actualizar'}), 404
+    @classmethod #Endpoint de Prueba http://127.0.0.1:5000/api/usuarios/1 METODO PUT
+    def actualizar_usuario(cls, user_id):
+        data = request.json
+        if Usuario.actualizar_usuario(user_id, data):
+            return jsonify({"message": "Usuario actualizado exitosamente"}), 200
+        else:
+            return jsonify({"message": "Error al actualizar el usuario"}), 500
 
-@usuario_bp.route('/usuarios/<int:user_id>', methods=['DELETE'])
-def eliminar_usuario(user_id):
-    if Usuario.eliminar_usuario(user_id):
-        return jsonify({'message': 'Usuario eliminado'}), 200
-    return jsonify({'message': 'Usuario no encontrado o error al eliminar'}), 404
+    @classmethod #Endpoint de Prueba http://127.0.0.1:5000/api/usuarios/1 METODO DELETE
+    def eliminar_usuario(cls, user_id):
+        if Usuario.eliminar_usuario(user_id):
+            return jsonify({"message": "Usuario eliminado exitosamente"}), 200
+        else:
+            return jsonify({"message": "Error al eliminar el usuario"}), 500
